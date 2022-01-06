9c37db7b550be87387e12e786c3e5d861b1938cc
PawelPamula1 <pawelpamula003@gmail.com>
2022-01-06 15:57:01 +0100

    building header

---
+++ src/Header.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import './Header.css';
+import SearchIcon from '@mui/icons-material/Search';
+import HomeIcon from '@mui/icons-material/Home';
+import PeopleIcon from '@mui/icons-material/People';
+import WorkIcon from '@mui/icons-material/Work';
+import ChatIcon from '@mui/icons-material/Chat';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import logo from './assets/img/logo-linkedin.webp';
+import photo from './assets/img/profile_photo.jpg';
+import HeaderOption from './HeaderOption';
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header__left">
+        <img src={logo} alt="logo" />
+        <div className="header__search">
+          <SearchIcon />
+          <input type="text" placeholder="Search" />
+        </div>
+      </div>
+
+      <div className="header__right">
+        <HeaderOption Icon={HomeIcon} title="Home" />
+        <HeaderOption Icon={PeopleIcon} title="My Network" />
+        <HeaderOption Icon={WorkIcon} title="Jobs" />
+        <HeaderOption Icon={ChatIcon} title="Messaging" />
+        <HeaderOption Icon={NotificationsIcon} title="Notifications" />
+        <HeaderOption avatar={photo} title="Me" />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
