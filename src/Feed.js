72b5b1cd87c8160a0ea8f17379a7b824b9a7529c
PawelPamula1 <pawelpamula003@gmail.com>
2022-01-07 22:33:45 +0100

    creating Post component

--- src/Feed.js
+++ src/Feed.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CreateIcon from '@mui/icons-material/Create';
 import ImageIcon from '@mui/icons-material/Image';
 import OndemandVideoIcon from '@mui/icons-material/OndemandVideo';
@@ -6,8 +6,11 @@ import EventIcon from '@mui/icons-material/Event';
 import ArticleIcon from '@mui/icons-material/Article';
 import './Feed.css';
 import InputOption from './InputOption';
+import Post from './Post';
 
 const Feed = () => {
+  const [posts, setPosts] = useState([]);
+
   return (
     <div className="feed">
       <div className="feed__inputContainer">
@@ -24,6 +27,10 @@ const Feed = () => {
           <InputOption title="Write Article" Icon={ArticleIcon} color="#FC9295" />
         </div>
       </div>
+      {posts.map((post) => (
+        <Post name={post.name} description={post.description} message={post.message} />
+      ))}
+      <Post name="Elon Musk" description="testowanie" message="Siemanko, kupiłem wszystkie bitkojny na świecie" />
     </div>
   );
 };
---
+++ src/Post.js
@@ -0,0 +1,31 @@
+import { Comment, Send, Share, ThumbUpAltOutlined } from '@mui/icons-material';
+import { Avatar } from '@mui/material';
+import InputOption from './InputOption';
+import './Post.css';
+
+const Post = ({ name, description, message, photoUrl }) => {
+  return (
+    <div className="post">
+      <div className="post__header">
+        <Avatar />
+        <div className="post__info">
+          <h2>{name}</h2>
+          <p>{description}</p>
+        </div>
+      </div>
+
+      <div className="post__body">
+        <p>{message}</p>
+      </div>
+
+      <div className="post__buttons">
+        <InputOption Icon={ThumbUpAltOutlined} title="Like" color="gray" />
+        <InputOption Icon={Comment} title="Comment" color="gray" />
+        <InputOption Icon={Share} title="Share" color="gray" />
+        <InputOption Icon={Send} title="Send" color="gray" />
+      </div>
+    </div>
+  );
+};
+
+export default Post;
